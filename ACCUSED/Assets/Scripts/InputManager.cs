928cfcfce82a9581c3b35aee1dd947543af1140a
noobie213 <78365976+noobie213@users.noreply.github.com>
2025-08-06 23:46:37 +0800

    BUSTO-003_Player-Movement-Update

--- ACCUSED/Assets/Scripts/InputManager.cs
+++ ACCUSED/Assets/Scripts/InputManager.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     private PlayerControls playerControls;
     private PlayerControls.OnFeetActions onFeet;
 
+    private PlayerMovement movement;
+
     // Start is called before the first frame update
     void Awake()
     {
         playerControls = new PlayerControls();
         onFeet = playerControls.OnFeet;
-        
+        movement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        movement.ProcessMove(onFeet.WASD.ReadValue<Vector2>());
     }
 
     private void OnEnable()
